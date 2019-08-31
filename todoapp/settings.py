3f1285d2e85e38a4112caf5861b15163885d0f59
mohtasheem <mohtasheem@gmail.com>
2019-08-30 22:06:53 -0400

    Update settings.py

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+ALLOWED_HOSTS = ['*']
 STATIC_URL = '/static/'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
